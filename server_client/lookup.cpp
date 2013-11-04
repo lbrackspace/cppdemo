e6741031330aa4af0a89bb76f60f99fd6f1fe1d3
Carlos D. Garza <carlos.garza@rackspace.com>
2013-11-04 17:32:23 -0600

    Got IPv6 to work on ubuntu.

--- server_client/lookup.cpp
+++ server_client/lookup.cpp
@@ -25,16 +25,32 @@ int main(int argc, char **argv) {
         return 0;
     }
     ip::tcp::resolver::iterator end;
+    int i = 0;
     while (*it != end) {
-        cout << (*it)->endpoint() << endl;
+        ip::tcp::endpoint ep((*it)->endpoint());
+        boost::asio::ip::address addr = ep.address();
+        cout << "endpoint[" << i << "]: " << address_to_string(addr) << " " << ep << endl;
         (*it)++;
+        i++;
     }
     return 0;
 }
 
+string address_to_string(ip::address& addr) {
+    ostringstream os;
+    os << "{"
+            << " is_loopback=" << boolalpha << addr.is_loopback() << ", "
+            << "is_multicase: " << boolalpha << addr.is_multicast() << ", "
+            << "is_unspecified: " << boolalpha << addr.is_unspecified() << ", "
+            << "is_v4: " << boolalpha << addr.is_v4() << ", "
+            << "is_v6: " << boolalpha << addr.is_v6() << ", "
+            << "addr: " << addr.to_string() << "}";
+    return os.str();
+}
+
 boost::shared_ptr<ip::tcp::resolver::iterator> resolveTCP(string& host, string& service, io_service& ios, boost::system::error_code& ec) {
     ip::tcp::resolver r(ios);
-    ip::tcp::resolver::query q(host, service);
+    ip::tcp::resolver::query q(host, service, ip::resolver_query_base::all_matching);
     boost::shared_ptr<ip::tcp::resolver::iterator> it(new ip::tcp::resolver::iterator(r.resolve(q, ec)));
     if (ec) {
         boost::shared_ptr<ip::tcp::resolver::iterator> null;
@@ -46,8 +62,9 @@ boost::shared_ptr<ip::tcp::resolver::iterator> resolveTCP(string& host, string& 
 }
 
 int usage(char *prog) {
+    bool t = true;
+    bool f = false;
     cout << "usage is " << prog << " <hostname> <port>" << endl;
     cout << endl;
-    cout << "Lookup the ip address of host machine" << endl;
     return 0;
 }
--- server_client/lookup.h
+++ server_client/lookup.h
@@ -3,5 +3,6 @@
 #include<boost/asio.hpp>
 #include<string>
 int usage(char *prog);
+std::string address_to_string(boost::asio::ip::address& addr);
 boost::shared_ptr<boost::asio::ip::tcp::resolver::iterator> resolveTCP(std::string& host, std::string& service, boost::asio::io_service& ios, boost::system::error_code& ec);
 #endif
